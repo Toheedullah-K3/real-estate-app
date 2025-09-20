cf6e89f842074ced9c7a809cc15f9f630dd5e9d2
Toheedullah-K3 <toheedullah002@gmail.com>
2025-09-20 19:42:56 +0500

    Added client folder as normal directory

---
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import Image from 'next/image'
+import React from 'react'
+import {motion} from "framer-motion"
+
+
+const HeroSection = () => {
+  return (
+    <div className='relative h-screen'>
+      <Image 
+        src= '/landing-splash.jpg'
+        alt='Rentiful Rental Platform Hero Section'
+        fill
+        className='object-cover object-center'
+        priority
+      />
+      <div className='absolute inset-0 bg-black bg-opacity-60'></div>
+
+      <motion.div
+        initial={{opacity:0, y:20}}
+        animate={{opacity: 1, y:0}}
+        transition={{duration:0.8}}
+        className='absolute top-1/3 transform -translate-x-1/2 -translate-y-1/2 text-center w-full'
+      >
+        <div className='max-w-4xl mx-auto px-16 sm:px-12'>
+            <h1 className='text-5xl text-white mb-4'>
+                Start your journet to finding the perfect place to call home
+            </h1>
+        </div>
+      </motion.div>
+    </div>
+  )
+}
+
+export default HeroSection
