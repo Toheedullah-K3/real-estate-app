8269e9646f2676211bd97b1b66514a199b645d6e
Toheedullah-K3 <toheedullah002@gmail.com>
2025-09-24 19:33:42 +0500

    authProvider added of aws nd amplify

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,6 +1,12 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";   
+import { dirname } from "path";        
+
+const __filename = fileURLToPath(import.meta.url);  
+const __dirname = dirname(__filename); 
+
 
 const prisma = new PrismaClient();
 
